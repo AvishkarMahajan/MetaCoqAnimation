82619082ad96e918bc7e89f72d94131eb5f76716
Avishkar Mahajan <avishkarmahajan@Avishkars-Laptop.local>
2025-06-04 22:45:09 +0800

    use section variables instead of parameters

--- theories/testCases.v
+++ theories/testCases.v
@@ -13,10 +13,10 @@ Local Open Scope nat_scope.
 Open Scope bs.
 
 (* 1 *)
-
-Parameter g1 : nat -> nat.
-Parameter g2 : nat -> nat.
-Parameter g3 : nat -> nat -> nat.
+Section s.
+Variable g1 : nat -> nat.
+Variable g2 : nat -> nat.
+Variable g3 : nat -> nat -> nat.
 
 Lemma beq_nat_eq : forall n m, true = (n =? m) -> n = m. Proof. Admitted.
 Lemma beq_nat_neq : forall n m, false = (n =? m) -> (n = m -> False). Proof. Admitted.
@@ -25,6 +25,8 @@ Lemma beq_nat_neq : forall n m, false = (n =? m) -> (n = m -> False). Proof. Adm
 (* a, b designated as input, c d e designated as output *)
 Inductive foo : nat -> nat -> nat -> nat -> nat -> Prop :=
  | cstr : forall a b c d e, (e = b /\ d = c /\ c = (g3 a e) /\ g1 d = g2 a) -> foo a b c d e.
+ 
+MetaRocq Run (general.animate <? foo ?>).
 
 
 
@@ -33,13 +35,17 @@ Inductive foo : nat -> nat -> nat -> nat -> nat -> Prop :=
 Definition soundness' (f : (nat -> nat -> option (list nat))) (n1 : nat) (n2 : nat) : Type :=
  let r := (f n1 n2) in 
    match r with
-    | Some ([n3 ; n4 ; n5]) => forall h1, forall h2, forall h3, h1 = g1 -> h2 = g2 -> h3 = g3 -> (foo n1 n2 n3 n4 n5) 
-    | None => forall h1, forall h2, forall h3, h1 = g1 -> h2 = g2 -> h3 = g3 ->  (forall n3 n4 n5 : nat, (foo n1 n2 n3 n4 n5 -> False))
+    | Some ([n3 ; n4 ; n5]) => (* forall h1, forall h2, forall h3, h1 = g1 -> h2 = g2 -> h3 = g3 -> *) (foo n1 n2 n3 n4 n5) 
+    | None => (forall n3 n4 n5 : nat, (foo n1 n2 n3 n4 n5 -> False))
  (*  (forall n3 n4 n5 : nat, (foo n1 n2 n3 n4 n5 -> False)) *)
     | _ => False
     end. 
 Definition soundness'' (f : (nat -> nat -> option (list nat))) : Type :=
- forall n1 n2, soundness' f n1 n2 .  
+ forall n1 n2, soundness' f n1 n2 .
+ 
+
+(* Check foo. 
+Check soundness''. *) 
  
   
 Definition animate'' (conjs : term) (inputVars : (list string)) (fuel : nat) : TemplateMonad unit :=
@@ -47,10 +53,11 @@ Definition animate'' (conjs : term) (inputVars : (list string)) (fuel : nat) : T
   f <- @tmUnquoteTyped (nat -> nat -> (option (list nat))) t' ;; tmPrint f ;;
   lemma1_name <- tmFreshName "lemma" ;;
   lemma1 <- tmQuote =<< tmLemma lemma1_name (soundness'' f) ;;
-  tmMsg "done".     
+  tmMsg "done".
+       
 
 
-MetaRocq Run (general.animate <? foo ?>).
+
 
 Definition fooTerm : term := 
  (tApp <%and%>
@@ -58,12 +65,8 @@ Definition fooTerm : term :=
     tApp <%and%>
       [tApp <%eq%> [<%nat%>; tVar "d"; tVar "c"];
        tApp <%and%>
-         [tApp <%eq%>
-            [<%nat%>; tVar "c";
-             tApp (tConst (MPfile ["testCases"; "Animation"], "g3") []) [tVar "a"; tVar "e"]];
-          tApp <%eq%>
-            [<%nat%>; tApp (tConst (MPfile ["testCases"; "Animation"], "g1") []) [tVar "d"];
-             tApp (tConst (MPfile ["testCases"; "Animation"], "g2") []) [tVar "a"]]]]]).
+         [tApp <%eq%> [<%nat%>; tVar "c"; tApp (tVar "g3") [tVar "a"; tVar "e"]];
+          tApp <%eq%> [<%nat%>; tApp (tVar "g1") [tVar "d"]; tApp (tVar "g2") [tVar "a"]]]]]).
 
 
 MetaRocq Run (animate'' fooTerm ["a" ; "b"] 10).
@@ -74,15 +77,72 @@ remember (Nat.eqb (g1 (g3 n1 n2)) (g2 n1)) as H. destruct H.
 + split.
 ++ (*apply cstr.*) apply beq_nat_eq in HeqH. rewrite -> HeqH.
 auto. 
-+ intros. inversion H2 ; subst. apply beq_nat_neq in HeqH.
++ intros. inversion H ; subst. apply beq_nat_neq in HeqH.
 *  auto.
-* destruct H3. rewrite H in H0. destruct H0.
-rewrite H0 in H1. destruct H1. rewrite H1 in H3. auto.
- Qed.    
+* destruct H0. rewrite H0 in H1. destruct H1.
+rewrite H1 in H2. destruct H2. rewrite H2 in H3. auto.
+ Qed.
+
+Inductive foo' : nat -> nat -> nat -> nat -> nat -> Prop :=
+ | cstr' : forall a b c d e, (g1 d = g2 a /\ d = c /\ c = (g3 a e) /\ e = b ) -> foo' a b c d e.
+ 
+MetaRocq Run (general.animate <? foo' ?>).
+
+Definition foo'Term : term := 
+ (tApp <%and%>
+   [tApp <%eq%> [<%nat%>; tApp (tVar "g1") [tVar "d"]; tApp (tVar "g2") [tVar "a"]];
+    tApp <%and%>
+      [tApp <%eq%> [<%nat%>; tVar "d"; tVar "c"];
+       tApp <%and%>
+         [tApp <%eq%> [<%nat%>; tVar "c"; tApp (tVar "g3") [tVar "a"; tVar "e"]];
+          tApp <%eq%> [<%nat%>; tVar "e"; tVar "b"]]]]).
+
+
+
+MetaRocq Run (animate'' foo'Term ["a" ; "b"] 30).
+
+Next Obligation.
+Proof. unfold soundness'. (* exists ((fun n1 n2 => if Nat.eqb (g1 (g3 n1 n2)) (g2 n1) then Some [g3 n1 n2; g3 n1 n2; n2] else None) n1 n2). *)
+remember (Nat.eqb (g1 (g3 n1 n2)) (g2 n1)) as H. destruct H.
++ split.
+++ (*apply cstr.*) apply beq_nat_eq in HeqH. rewrite -> HeqH.
+auto. 
++ intros. inversion H ; subst. apply beq_nat_neq in HeqH.
+*  auto.
+* destruct H0. rewrite H0 in H1. destruct H1.
+rewrite H1 in H2. destruct H2. rewrite H2 in H3. auto.
+ Qed.
+     
+(* Failing test case : conjunct b = e cannot be animated since known var 'b' is on LHS not RHS *)
+
+
+
+Inductive foo'' : nat -> nat -> nat -> nat -> nat -> Prop :=
+ | cstr'' : forall a b c d e, (g1 d = g2 a /\ d = c /\ c = (g3 a e) /\ b = e ) -> foo'' a b c d e.
+ 
+MetaRocq Run (general.animate <? foo'' ?>).
+
+Definition foo''Term : term := 
+(tApp <%and%>
+   [tApp <%eq%> [<%nat%>; tApp (tVar "g1") [tVar "d"]; tApp (tVar "g2") [tVar "a"]];
+    tApp <%and%>
+      [tApp <%eq%> [<%nat%>; tVar "d"; tVar "c"];
+       tApp <%and%>
+         [tApp <%eq%> [<%nat%>; tVar "c"; tApp (tVar "g3") [tVar "a"; tVar "e"]];
+          tApp <%eq%> [<%nat%>; tVar "b"; tVar "e"]]]]).
+
+(* Fails with : no such var d  
+MetaRocq Run (animate'' foo''Term ["a" ; "b"] 30).*)
+
+End s.
+
+          
+
+
  
   
 
-(* 2 *)
+
 
 Inductive tuple : nat -> nat -> (prod nat nat) -> Prop :=
  | tupleCon : forall (a : nat), forall (b : nat), forall (y : (prod nat nat)), (a, S b) = y -> tuple a b y.  (*RHS of equality not v imp*)
@@ -92,7 +152,7 @@ MetaRocq Quote Recursively Definition tupleTerm := tuple.
 
 MetaRocq Run (t <- tmEval all  (typeConstrPatMatch.removeopTm (DB.deBruijnOption ((typeConstrPatMatch.removeopTm (typeConstrPatMatch.mkLamfromInd tupleTerm 25))))) ;; tmUnquote t >>= tmPrint).
 
-(* 3 *)
+
 
 Inductive triple' : nat -> list nat -> Prop :=
  | triple'Con : forall (a : nat), forall (y : list nat), (a :: [])  = y -> triple' a  y.  (*RHS of equality not v imp*)
@@ -120,70 +180,16 @@ MetaRocq Quote Recursively Definition baz'Term := baz'.
 
 MetaRocq Run (t <- tmEval all  (typeConstrPatMatch.removeopTm (DB.deBruijnOption ((typeConstrPatMatch.removeopTm (typeConstrPatMatch.mkLamfromInd baz'Term 30))))) ;; tmUnquote t >>= tmPrint).
 
-(* 5 *)
-
-Inductive foo' : nat -> nat -> nat -> nat -> nat -> Prop :=
- | cstr' : forall a b c d e, (g1 d = g2 a /\ d = c /\ c = (g3 a e) /\ e = b ) -> foo' a b c d e.
- 
-MetaRocq Run (general.animate <? foo' ?>).
-
-Definition foo'Term : term := 
- (tApp <%and%>
-   [tApp <%eq%>
-      [<%nat%>; tApp (tConst (MPfile ["testCases"; "Animation"], "g1") []) [tVar "d"];
-       tApp (tConst (MPfile ["testCases"; "Animation"], "g2") []) [tVar "a"]];
-    tApp <%and%>
-      [tApp <%eq%> [<%nat%>; tVar "d"; tVar "c"];
-       tApp <%and%>
-         [tApp <%eq%>
-            [<%nat%>; tVar "c";
-             tApp (tConst (MPfile ["testCases"; "Animation"], "g3") []) [tVar "a"; tVar "e"]];
-          tApp <%eq%> [<%nat%>; tVar "e"; tVar "b"]]]]).
 
 
-MetaRocq Run (animate'' foo'Term ["a" ; "b"] 30).
-
-Next Obligation.
-Proof. unfold soundness'. (* exists ((fun n1 n2 => if Nat.eqb (g1 (g3 n1 n2)) (g2 n1) then Some [g3 n1 n2; g3 n1 n2; n2] else None) n1 n2). *)
-remember (Nat.eqb (g1 (g3 n1 n2)) (g2 n1)) as H. destruct H.
-+ split.
-++ (*apply cstr.*) apply beq_nat_eq in HeqH. rewrite -> HeqH.
-auto. 
-+ intros. inversion H2 ; subst. apply beq_nat_neq in HeqH.
-*  auto.
-* destruct H3. rewrite H in H0. destruct H0.
-rewrite H0 in H1. destruct H1. rewrite H1 in H3. auto.
- Qed.  
 
 
-(* 6 *) (* Failing test case : conjunct b = e cannot be animated since known var 'b' is on LHS not RHS *)
 
-Inductive foo'' : nat -> nat -> nat -> nat -> nat -> Prop :=
- | cstr'' : forall a b c d e, (g1 d = g2 a /\ d = c /\ c = (g3 a e) /\ b = e ) -> foo'' a b c d e.
- 
-MetaRocq Run (general.animate <? foo'' ?>).
 
-Definition foo''Term : term := 
- (tApp <%and%>
-   [tApp <%eq%>
-      [<%nat%>; tApp (tConst (MPfile ["testCases"; "Animation"], "g1") []) [tVar "d"];
-       tApp (tConst (MPfile ["testCases"; "Animation"], "g2") []) [tVar "a"]];
-    tApp <%and%>
-      [tApp <%eq%> [<%nat%>; tVar "d"; tVar "c"];
-       tApp <%and%>
-         [tApp <%eq%>
-            [<%nat%>; tVar "c";
-             tApp (tConst (MPfile ["testCases"; "Animation"], "g3") []) [tVar "a"; tVar "e"]];
-          tApp <%eq%> [<%nat%>; tVar "b"; tVar "e"]]]]).
 
- 
 
-(* Fails with : no such var d 
-MetaRocq Run (animate'' foo''Term ["a" ; "b"] 30).
 
-*) 
 
-(* 7 *)
 
 Inductive fooCon : nat -> nat -> nat -> nat -> Prop :=
  | cstrCon : forall a b c d,  [1 ; S c]  = [S b ; d]  -> fooCon a b c d.
@@ -240,7 +246,7 @@ Compute (typeConstrReduce.makeConjSimpl (typeConstrReduce.deconTypeConGen'' (typ
 
 (* Returns conjuncts corresponding to : 0 = b , S c = d *)
 
-(* 8 *)
+
 
 Inductive fooCon' : nat -> nat -> nat -> nat -> Prop :=
  | cstrCon' : forall a b c d, [S b ; d] = [1 ; S c]  -> fooCon' a b c d.
